48d9642d3c5516eb36bf11cfd6ce8dc71c793727
Felilpz <luizfelipe.vieira12@gmail.com>
2023-09-26 14:03:24 -0300

    wip

---
+++ app.py
@@ -0,0 +1,132 @@
+from flask import Flask, request, jsonify, send_file
+from psycopg2 import connect, extras
+
+
+app = Flask(__name__)
+
+host = 'localhost'
+port = 5432
+dbname = 'testinho'
+username = 'postgres'
+password = 123
+
+def conectando():
+    conn = connect(host=host, port=port, dbname=dbname, user=username, password=password)
+    return conn
+
+
+
+@app.get('/api/transacoes')
+def pegar_transacoes():
+    conn = conectando()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    
+    cur.execute("SELECT * FROM transacoes")
+    transacoes = cur.fetchall()
+
+    cur.close()
+    conn.close()
+    
+    return jsonify(transacoes)
+    
+    # return 'pegando transacao'
+
+
+
+@app.post('/api/transacoes')
+def criar_transacoes():
+    nova_transacao = request.get_json()
+    descricao = nova_transacao['descricao']
+    valor = nova_transacao['valor']
+    tipo = nova_transacao['tipo']
+
+    conn = conectando()
+    cur = conn.cursor()
+    
+    cur.execute('INSERT INTO transacoes(descricao, valor, tipo) VALUES (%s, %s, %s) RETURNING *', (descricao, valor, tipo))
+    novo_usuario_criado = cur.fetchone()
+    print(novo_usuario_criado)
+    conn.commit()
+    
+    conn.close()
+    cur.close()
+    
+    return jsonify(novo_usuario_criado)
+    
+    
+    
+@app.delete('/api/transacoes/<idtransacao>')
+def deletar_transacoes(idtransacao):
+    conn = conectando()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    cur.execute('DELETE FROM transacoes WHERE idTransacao = %s RETURNING * ', (idtransacao,))
+    
+    transacao = cur.fetchone()
+
+    print(transacao)
+
+    conn.commit()
+
+    conn.close()
+    cur.close()
+
+    if transacao is None:
+        return jsonify({'message': 'Transacão não econtrada'}), 404
+    
+    return jsonify(transacao)
+
+
+
+@app.put('/api/transacoes/<idtransacao>')
+def put_transacoes(idtransacao):
+    
+    conn = conectando()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    novo_usuario = request.get_json()
+    descricao = novo_usuario['descricao'] 
+    valor = novo_usuario['valor'] 
+    tipo = novo_usuario['tipo'] 
+    
+    cur.execute('UPDATE transacoes SET descricao = %s, valor = %s, tipo = %s WHERE idtransacao = %s RETURNING *', (descricao, valor, tipo, idtransacao))
+    usuario_atualizado = cur.fetchone()
+    
+    conn.commit()
+    
+    cur.close()
+    conn.close()
+    
+    if usuario_atualizado is None:
+        return jsonify({"message": "Transação não encontrada"}), 404 
+    
+    return jsonify(usuario_atualizado)
+
+
+
+
+@app.get('/api/transacoes/<idtransacao>')
+def pegar_transacao(idtransacao):
+    print(idtransacao)
+    conn = conectando()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    cur.execute('SELECT * FROM transacoes WHERE idTransacao = %s', (idtransacao,))
+    transacao = cur.fetchone()
+
+    cur.close()
+    conn.close()
+    
+    if transacao is None:
+        return jsonify({'message': 'Transacão não econtrada'}), 404
+    
+    
+    
+@app.get('/')    
+def home():
+    return send_file('static/index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+    else:
+        return jsonify({'error': 'Transação não encontrada'}), 404
